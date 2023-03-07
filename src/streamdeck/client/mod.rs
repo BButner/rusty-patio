d79d5b98a542dd48a1befefb3f6411454889706d
BButner <butner.beau@gmail.com>
2023-03-07 09:05:31 -0600

    Send image path instead of raw bytes

--- src/streamdeck/client/mod.rs
+++ src/streamdeck/client/mod.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use base64::{engine::general_purpose, Engine as _};
 use serde::Serialize;
 
 use crate::payloads::{
@@ -47,13 +48,24 @@ impl StreamDeckClient {
     pub async fn set_image(
         &mut self,
         context: String,
-        image: String,
+        image_path: String,
         target: StreamDeckTarget,
         state: Option<u8>,
     ) -> Result<()> {
-        self.send_json_message(StreamDeckSetImageMessage::new(
-            context, image, target, state,
-        ))
-        .await
+        let image_bytes = tokio::fs::read(&image_path).await?;
+        let encoded = general_purpose::STANDARD.encode(&image_bytes);
+        let mime_type = mime_guess::from_path(&image_path).first();
+
+        if let Some(mime_type) = mime_type {
+            self.send_json_message(StreamDeckSetImageMessage::new(
+                context,
+                format!("data:{};base64,{}", mime_type, encoded),
+                target,
+                state,
+            ))
+            .await
+        } else {
+            Err(anyhow::anyhow!("Could not determine mime type"))
+        }
     }
 }
